ff09c5ab84299fde62a9f89266776639df28e32c
JakeNicks <jakenichols57@yahoo.com>
2023-04-03 10:46:02 -0500

    updating graph and formatting

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "GOP-Metro_Whole_Modularity.gexf",
+    graphFile : "GOP-Metro_Whole_modularity_v2.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
